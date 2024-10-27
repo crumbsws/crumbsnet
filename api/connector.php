736d22d00e63f8b992afbc08eb7e0a089874c0cb
crumbsws <greenvibezz993@gmail.com>
2024-10-27 09:36:10 +0000

    first commit

---
+++ api/connector.php
@@ -0,0 +1,8 @@
+<?php 
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Origin: https://crumbs.lol');
+header('Access-Control-Allow-Headers: Content-Type');
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+$conn = mysqli_connect();
+?>
+
