6648745b44529bcbad573be18c9fc7aaddefbd8a
crumbsws <greenvibezz993@gmail.com>
2024-10-27 09:42:14 +0000

    first commit 4

--- api/connector.php
+++ api/connector.php
@@ -1,1 +1,9 @@
+<?php 
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Origin: https://crumbs.lol');
+header('Access-Control-Allow-Headers: Content-Type');
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+$conn = mysqli_connect('db-mysql-fra1-51403-do-user-17164387-0.f.db.ondigitalocean.com:25060', 'doadmin', 'AVNS_4ar3mUrmBSvDyBAiynU'>
+?>
+
 
