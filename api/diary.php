f1bf5293f48c21a8d1c7c0b7081f364675843cf9
Crumbs WS <98031676+crumbsws@users.noreply.github.com>
2025-01-09 23:41:00 +0300

    dupedeletors

--- api/diary.php
+++ api/diary.php
@@ -17,12 +17,20 @@ if(!empty($_POST['message']))
     $amount = 1;
     $date = date("Y-m-d h:i");
     $message = mysqli_real_escape_string($conn, $_POST['message']);
-    $sql = "INSERT INTO diary (name, message, date) VALUES ('$user', '$message', '$date')";
+    $sql = "DELETE FROM diary WHERE name='$user'";
     if(mysqli_query($conn, $sql)){
-    $state= 'success';
-    $message= 'Diary Updated';
-    setResponse($state, $message);
-    addPoint($conn, $user, $amount);
+        $sql = "INSERT INTO diary (name, message, date) VALUES ('$user', '$message', '$date')";    
+        if(mysqli_query($conn, $sql)){
+            $state= 'success';
+            $message= 'Diary Updated';
+            setResponse($state, $message);
+            addPoint($conn, $user, $amount);
+        }
+        else {
+            $state= 'fail';
+            $message= 'Failed';
+            setResponse($state, $message);
+        }  
     }
     else {
         $state= 'fail';
--- api/reactPost.php
+++ api/reactPost.php
@@ -16,14 +16,19 @@ if(!empty($data['value']) && !empty($data['url']))
 {
     $user = $_SESSION['user'];
     $url = $data['url'];
-    $amount = 1;
     $date = date("Y-m-d h:i");
     $rating = mysqli_real_escape_string($conn, $data['value']);
-    $sql = "INSERT IGNORE INTO reaction (name, rating, url, date) VALUES ('$user', '$rating', '$url', '$date')";
+    $sql = "DELETE FROM reaction WHERE name='$user' AND url='$url'";
     if(mysqli_query($conn, $sql)){
-    $state= 'success';
-    setResponse($state);
-    addPoint($conn, $user, $amount);
+        $sql = "INSERT IGNORE INTO reaction (name, rating, url, date) VALUES ('$user', '$rating', '$url', '$date')";
+        if(mysqli_query($conn, $sql)){
+            $state= 'success';
+            setResponse($state);
+        }
+        else {
+            $state= 'fail';
+            setResponse($state);
+        } 
     }
     else {
         $state= 'fail';
