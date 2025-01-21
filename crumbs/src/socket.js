513dea6511b709103b125a576fb137345e38e7ad
Crumbs WS <98031676+crumbsws@users.noreply.github.com>
2025-01-21 16:46:13 +0300

    Update socket.js

--- crumbs/src/socket.js
+++ crumbs/src/socket.js
@@ -3,4 +3,8 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 const url = process.env.REACT_APP_SOCKET_URL;
 
-export const socket = io(url);
+export const socket = io(url, {
+    path: process.env.REACT_APP_SOCKET_PATH,
+    transports: ["websocket"],
+    withCredentials: true,
+}););
